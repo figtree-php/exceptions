84b6d51e059cbeeee1cc216cedc3a4acb18ea56b
Alan Ondra <r.alan.ondra@gmail.com>
2021-04-30 12:58:28 -0400

    Remove test artifacts and risky behaviors.

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -3,16 +3,8 @@
 namespace FigTree\Exceptions\Tests;
 
 use PHPUnit\Framework\TestCase;
-use FigTree\Exceptions\Tests\Concerns\ReadsTestData;
 
 abstract class AbstractTestCase extends TestCase
 {
-	use ReadsTestData;
-
-	public function __construct()
-	{
-		parent::__construct();
-
-		$this->setDataDir(__DIR__ . '/Data');
-	}
+	//
 }
